861f10e3fb266e382befbda77e37f377e387eed1
Samuel <sacorder@student.42madrid.com>
2024-02-04 17:59:31 +0100

    ex00 done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,19 +6,30 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/04 17:15:57 by sacorder          #+#    #+#             */
-/*   Updated: 2024/02/04 17:20:51 by sacorder         ###   ########.fr       */
+/*   Updated: 2024/02/04 17:58:55 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+std::string	toUpperString(char *cstr)
+{
+	std::string str;
+
+	for (int i = 0; cstr[i]; ++i)
+		str += toupper(cstr[i]);
+	return (str);
+}
+
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
-	for (int i = 1; i < argc; ++i)
 	{
-		std::cout << argv[i] << std::endl;
+		std::cout << "LOUD AND UNBEARABLE FEEDBACK NOISE" << std::endl;
+		return (0);
 	}
+	for (int i = 1; i < argc; ++i)
+		std::cout << toUpperString(argv[i]);
+	std::cout << std::endl;
 	return (0);
 }
