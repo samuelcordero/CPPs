2a4a1a86d82d66dc469fee006e91c60c0c009cb8
Samuel <sacorder@student.42madrid.com>
2024-03-04 13:39:38 +0100

    check operators

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,14 +6,14 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 12:41:30 by sacorder          #+#    #+#             */
-/*   Updated: 2024/03/04 13:22:24 by sacorder         ###   ########.fr       */
+/*   Updated: 2024/03/04 13:39:32 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
-
 #include <iostream>
-	int main(void) {
+
+int main(void) {
 	Fixed a;
 	Fixed const b(Fixed(5.05f) * Fixed(2));
 
