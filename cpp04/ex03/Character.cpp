44bd239df6804d20b491600f5943cddf5dde79ba
Samuel <sacorder@student.42madrid.com>
2024-04-05 14:06:52 +0200

    improved cons/dest msgs, add more tests

--- cpp04/ex03/Character.cpp
+++ cpp04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 01:06:46 by sacorder          #+#    #+#             */
-/*   Updated: 2024/04/05 13:47:39 by sacorder         ###   ########.fr       */
+/*   Updated: 2024/04/05 13:57:50 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,10 @@ Character::Character(const Character &cp) {
 
 Character::~Character() {
 	std::cout << "Default character dest\n";
-	/* for (int i = 0; i < 4; ++i) {
+	for (int i = 0; i < 4; ++i) {
 		if (inventory[i] != NULL)
 			delete inventory[i];
-	} */
+	}
 }
 
 Character	&Character::operator=(const Character &cp) {
--- cpp04/ex03/MateriaSource.cpp
+++ cpp04/ex03/MateriaSource.cpp
@@ -6,23 +6,26 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 01:19:57 by sacorder          #+#    #+#             */
-/*   Updated: 2024/04/05 13:50:47 by sacorder         ###   ########.fr       */
+/*   Updated: 2024/04/05 14:04:09 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MateriaSource.hpp"
 
 MateriaSource::MateriaSource() {
+	std::cout << "Materia source default cons\n";
 	for (int i = 0; i < 4; ++i)
 		known_materias[i] = NULL;
 }
 
 MateriaSource::MateriaSource(const MateriaSource &cp) {
+	std::cout << "Materia source copy cons\n";
 	for (int i = 0; i < 4; ++i)
 		known_materias[i] = cp.known_materias[i];
 }
 
 MateriaSource::~MateriaSource() {
+	std::cout << "Materia source dest\n";
 	for (int i = 0; i < 4; ++i) {
 		if (known_materias[i])
 			delete known_materias[i];
@@ -41,7 +44,6 @@ void	MateriaSource::learnMateria(AMateria *m) {
 	for (int i = 0; i < 4; ++i) {
 		if (known_materias[i] == NULL) {
 			known_materias[i] = m;
-			std::cout << "learned " << known_materias[i]->getType() << " at pos " << i << std::endl;
 			break ;
 		}
 	}
