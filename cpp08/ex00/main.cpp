cd83472c8a49fc6c8663af9f38a676e75f625e32
Samuel Cordero Rodrigo <sacorder@c1r2s4.42madrid.com>
2024-09-01 13:34:50 +0200

    cpp08: ex00: added list test to main

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sacorder <sacorder@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 12:26:40 by sacorder          #+#    #+#             */
-/*   Updated: 2024/09/01 13:23:41 by sacorder         ###   ########.fr       */
+/*   Updated: 2024/09/01 13:34:14 by sacorder         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,28 @@ int	main() {
 	test.push_back(3);
 	try {		
 		n = easyfind(test, 2);
-		std::cerr << "found at " << n << std::endl;
+		std::cout << "found at " << n << std::endl;
 		n = easyfind(test, 3);
-		std::cerr << "found at " << n << std::endl;
+		std::cout << "found at " << n << std::endl;
 		n = easyfind(test, 25);
-		std::cerr << "found at " << n << std::endl;
+		std::cout << "found at " << n << std::endl;
+	} catch (std::exception &e) {
+		std::cerr << "Exception: " << e.what() << std::endl;
+	}
+	std::cout << "List test:\n";
+	std::list<int> testl;
+	testl.push_back(4);
+	testl.push_back(2);
+	testl.push_back(231);
+	testl.push_back(2);
+	testl.push_back(3);
+	try {		
+		n = easyfind(testl, 2);
+		std::cout << "found at " << n << std::endl;
+		n = easyfind(testl, 3);
+		std::cout << "found at " << n << std::endl;
+		n = easyfind(testl, 25);
+		std::cout << "found at " << n << std::endl;
 	} catch (std::exception &e) {
 		std::cerr << "Exception: " << e.what() << std::endl;
 	}
